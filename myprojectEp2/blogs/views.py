202692769664bb3904513a5d2f132499dcfbdd45
Nosala-888 <Kongchauh@gmail.com>
2022-11-21 15:00:41 +0700

    Upload new
    dok

--- myprojectEp2/blogs/views.py
+++ myprojectEp2/blogs/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from category.models import Category
+from .models import Blogss
+from django.core.paginator import Paginator , EmptyPage , InvalidPage
 
 # Create your views here.
 def index(request):
-    return render(request,"frontend/index.html")
+    categories = Category.objects.all()
+    blogs=Blogss.objects.all()
+    latest = Blogss.objects.all().order_by('-pk')[:4]
+
+    paginator = Paginator(blogs,3)
+    try:
+        page = int(request.GET.get('page','1'))
+    except :
+        page = 1
+    
+    try :
+        blogPerpage = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        blogPerpage = paginator.page(paginator.num_pages)
+
+    return render(request,"frontend/index.html", {
+        'categories':categories,
+        'blogs':blogPerpage,
+        'latest':latest
+    })
+
+
+def blogDetail(request,id):
+    categories = Category.objects.all()
+    popular = Blogss.objects.all().order_by('-views')[:3]
+    suggestion = Blogss.objects.all().order_by('views')[:3]
+    singleBlog = Blogss.objects.get(id=id)
+    singleBlog.views = singleBlog.views+1
+    singleBlog.save()
+    return render(request,"frontend/blogDetail.html",{"blog":singleBlog,'categories':categories,'popular':popular,'suggestion':suggestion})
--- myprojectEp2/myproject/settings.py
+++ myprojectEp2/myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'category'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblogs',
+        'USER': 'root',
+        'PASSWORD':'',
     }
 }
 
